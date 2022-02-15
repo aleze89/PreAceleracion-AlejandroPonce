b8d59b8d7dac4904c11ec7d09f966158c62bb26f
aleze89 <68794504+aleze89@users.noreply.github.com>
2022-02-15 00:22:06 -0300

    error al crear la migracion, problemas al cargar las claves de la DB en las credenciales

--- PreAceleracionFebrero/Contexts/DisneyContext.cs
+++ PreAceleracionFebrero/Contexts/DisneyContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PreAceleracionFebrero.Models;
+using Microsoft.EntityFrameworkCore.SqlServer;
 
 namespace PreAceleracionFebrero.Contexts
 {
--- PreAceleracionFebrero/Program.cs
+++ PreAceleracionFebrero/Program.cs
@@ -1,3 +1,6 @@
+using PreAceleracionFebrero.Contexts;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
+builder.Services.AddEntityFrameworkSqlServer();
+builder.Services.AddDbContext<DisneyContext>((services, options) =>
+{
+    options.UseInternalServiceProvider(services);
+    options.UseSqlServer("Data Source=DESKTOP-UJVMHBA;Database=DisneyDb;Initial Catalog=NUEVA;Integrated Security=True;");
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
